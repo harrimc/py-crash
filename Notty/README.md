d28689aa6f60c5f1dd64fae93817b5a55a366a5f
harrimc <harrisonjohnmccarthy@gmail.com>
2025-09-20 22:04:24 +0100

    Lesson 1 Arithmetic and Variables

---
+++ Notty/README.md
@@ -0,0 +1,2 @@
+ ## day 1 
+lesson 1 Arithmetic variables, no roadblocks
---
+++ intro to programming/Arithmetic_variables.py
@@ -0,0 +1,10 @@
+#time calculator
+seconds = 60
+hours = 24
+minutes = 60
+days = 365.25
+babies_min = 250 
+
+babies_peryear = babies_min * hours * days*minutes 
+
+print(babies_peryear)
